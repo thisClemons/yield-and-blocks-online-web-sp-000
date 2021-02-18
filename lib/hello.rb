d69127817666f33bb8c7c5a4d42094925dbf07cf
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-18 17:28:32 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -8,9 +8,9 @@ def hello_t(array)
   array
 end
 
-call your method here!
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
+# call your method here!
+# hello_t(["Tim", "Tom", "Jim"]) do |name|
+#   if name.start_with?("T")
+#     puts "Hi, #{name}"
+#   end
+# end
